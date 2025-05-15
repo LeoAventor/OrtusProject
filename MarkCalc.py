41ad823004cb994d7aa917db6645d4b5656392f7
OverX25 <natanael.moroz@gmail.com>
2025-05-16 02:32:10 +0300

    yes.

--- MarkCalc.py
+++ MarkCalc.py
@@ -19,7 +19,7 @@ password_field.send_keys(os.environ.get("password"))
 submit_button = driver.find_element(By.NAME, "Login.Submit")
 submit_button.click()
 driver.get("https://estudijas.rtu.lv/my/")
-visible_course_div = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='course_div' and not(@style='display: none;')]")))
+visible_course_div = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "course_div")))
 course_links = visible_course_div.find_elements(By.TAG_NAME, "a")
 
 for link in course_links:
