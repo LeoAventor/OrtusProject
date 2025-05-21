868a1df537ac3fad92afba1b7d6088e4ce5d7b11
LeoAventor <grandizer65@gmail.com>
2025-05-22 01:46:08 +0300

    modificēta priekšmetu klases loģika

--- MarkCalc.py
+++ MarkCalc.py
@@ -125,31 +125,77 @@ class DatuStrukturas(Course):
 
 class DiskretaMatematika(Course):
     def calculate_final(self):
-        K = [self.grades.get("Kontroldarbs Nr 1", 0),
-             self.grades.get("Kontroldarbs Nr 2", 0),
-             self.grades.get("Kontroldarbs Nr 3", 0)]
-        M = self.grades.get("Mājasdarbi", 0)
-        T = [self.grades.get("2025.g. Tests 1.1 \"Darbības ar kopām\" ( OBLIGĀTAIS ) - līdz 15.03.2025", 0),
-             self.grades.get("Tests 2", 0),
-             self.grades.get("Testi", 0),
-             self.grades.get("Testi", 0)]
-        E = self.grades.get("Eksāmens", 0)
-        K = statistics.fmean(K)
+        print(self.grades)
+        K1 = self.grades.get("1. starpeksāmens - Kopas, attēlojumi, attieksmes.", 0)
+        
+        K2 =self.grades.get("2. starpeksāmens - Kombinatorika.", 0)
 
-        if K1 < 4 or K2 < 4 or K3 < 4:
-            self.final_grade = round(0.5 * E + 0.3 * K + 0.1 * M + 0.1 * T, 2)
+        K3 = self.grades.get("3. starpeksāmens - Būla funkcijas.", 0)
+        
+        
+        M = [self.grades.get("MD \"Matemātiskā indukcija\"", 0),
+             self.grades.get("MD \"Stingra sakārtojuma algoritms\"", 0),
+             self.grades.get("MD \"Būla funkcijas\"", 0)]
+        
+        T = [self.grades.get("2025.g. Tests 1.1 \"Darbības ar kopām\" ( OBLIGĀTAIS ) - līdz 15.03.2025", 0),
+             self.grades.get("2025.g. Tests 1.2 \"Kopu attēlojumi un attieksmes\" ( OBLIGĀTAIS ) - līdz 15.03.2025", 0),
+             self.grades.get("2025.g. Teorijas tests \"Kopu teorija\" ( OBLIGĀTAIS ) - līdz 15.Mar.2025", 0),
+             self.grades.get("Teorijas tests \"Kombinatorika\" ( OBLIGĀTAIS ) - līdz 23.Apr.2025", 0),
+             self.grades.get("2.2. Izlases ( OBLIGĀTAIS ) - līdz 23.04.2025", 0),
+             self.grades.get("7. Pilnās sistēmas ( OBLIGĀTAIS ) - līdz 21.05.2025", 0),
+             self.grades.get("Teorijas tests \"Matemātiskā loģika\" ( OBLIGĀTAIS ) - līdz 29.05.2025", 0)]
+        
+        
+        M = statistics.fmean(M)
+        T = statistics.fmean(T)
+
+        if K1 >= 4 and K2 >= 4 and K3 >= 4:
+
+            groups = [ K1, K2, K3 ,M, T,]
+            weights= [0.3, 0.2, 0.3, 0.1, 0.1]
+
         else:
-            self.final_grade = round(0.3 * K1 + 0.2 * K2 + 0.3 * K3 + 0.1 * M + 0.1 * T, 2)
+            E = self.grades.get("Eksāmens", 0)
+            groups = [K1, K2, K3, M, T, E ]
+            weights= [0.1, 0.1, 0.1, 0.1, 0.1, 0.5]
+
+        final = statistics.fmean(groups,weights)
+        return final
+
+           
 
 class Fizika(Course):
     def calculate_final(self):
-        LB = self.grades.get("Laboratorijas darbi", 0)
-        KD = self.grades.get("Kontroldarbi", 0)
-        Eks = self.grades.get("Eksāmens", 0)
+        print(self.grades)
+
+        LD = self.grades.get("Lab. Darbi", 0)
+        print(LD)
+
+        KDteor = [self.grades.get("I teorijas KD", 0),
+                  self.grades.get("II teorijas KD", 0)]
+        
+        print(KDteor)
+        KDprakt = [self.grades.get("P KD I", 0),
+                   self.grades.get("P KD II", 0)]
+        print(KDprakt)
+        
+        E = self.grades.get("Eksāmens", 0)
+        print(E)
+
+        LD = statistics.fmean(LD)
+        KDteor = statistics.fmean(KDteor)
+        KDprakt = statistics.fmean(KDprakt)
+
+        groups = [LD, KDteor, KDprakt, E]
+        final = statistics.fmean(groups, weights)
+        return final
+
+
         # pārbaudām vai ir kāda zem 4
-        low_score = any(g < 4 for g in [LB, KD, Eks] if g > 0)
+        low_score = LD >=4 or min(KDteor)>=4 or min(KDprakt) or E >=4
         if low_score:
-            self.final_grade = round(0.25 * LB + 0.25 * KD + 0.5 * Eks, 2)
+            KDteor and KDprakt = KD # ??????
+            weights = [0.25, 0.25, 0.5] # ???
         else:
             KDteor = self.grades.get("KD teorētiskie", 0)
             KDprakt = self.grades.get("KD praktiskie", 0)
@@ -158,31 +204,151 @@ class Fizika(Course):
 
 class Matematika(Course):
     def calculate_final(self):
-        E = self.grades.get("Eksāmens", 0)
-        K = self.grades.get("Kontroldarbi", 0)
-        M = self.grades.get("Mājasdarbi", 0)
-        L = self.grades.get("Laboratorijas darbi", 0)
-        T = self.grades.get("Testi", 0)
-        self.final_grade = round(0.5 * E + 0.25 * K + 0.1 * M + 0.1 * L + 0.05 * T, 2)
+        print(self.grades)
+        K = [self.grades.get("1.kontroldarba \"Integrēšanas ābece\" rezultāti", 0),
+            self.grades.get("2.kontroldarba \"Integrēšanas metodes\" rezultāti", 0),
+            self.grades.get("3.kontroldarba \"Noteiktais un neīstais integrālis\" rezultāti", 0),
+            self.grades.get("4.kontroldarba \"Diferenciālvienādojumi\" rezultāti", 0),
+            self.grades.get("5.kontroldarba \"Rindas\" rezultāti", 0)]
+        
+        print(K)
+
+        M = [self.grades.get("1.mājasdarba \"Nenoteiktais integrālis\" rezultāti", 0),
+             self.grades.get("2.mājasdarba \"Noteiktais integrālis, tā pielietojumi, neīstie integrāļi\" rezultāti", 0),
+             self.grades.get("3.mājasdarba \"Diferenciālvienādojumi\" rezultāti", 0),
+             self.grades.get("4.mājasdarba \"Rindas\" rezultāti", 0)]
+        
+        print(M)
+
+        L = [self.grades.get("1.laboratorijas darba rezultāti", 0),
+             self.grades.get("2.laboratorijas darba rezultāti", 0),
+             self.grades.get("3.laboratorijas darba rezultāti", 0)]
+        
+        print(L)
+
+        T = [self.grades.get("Tests 2.1. Noteiktā integrāļa aprēķināšana", 0),
+             self.grades.get("Tests 2.2 Plaknes figūras laukuma aprēķināšana", 0),
+             self.grades.get("Tests 2.3 Neīstie integrāļi", 0),
+             self.grades.get("1. teorijas tests", 0),
+             self.grades.get("2. teorijas tests", 0)]
+        
+        print(T)
+
+        S = [self.grades.get("1.starpeksāmena rezultāti", 0),
+             self.grades.get("2.starpeksāmena rezultāti", 0)]
+
+        print(S)
+
+        print(E) # ????
+
+        K=statistics.fmean
+        M=statistics.fmean
+        L=statistics.fmean
+        T=statistics.fmean
+        
+        groups = [E, K, M, L, T]
+        weights = [0.5, 0.25, 0.1, 0.1, 0.05]
+        final = statistics.fmean(groups, weights)
+        return final
+
+        # if min(S) >= 4:
+        #     E=statistics.fmean(S)
+        # else:
+        #     E = self.grades.get("Eksāmens", 0)
+        # self.final_grade = round(0.5 * E + 0.25 * K + 0.1 * M + 0.1 * L + 0.05 * T, 2) # jāizdomā ko darīt ar S
 
 class OOP(Course):
     def calculate_final(self):
-        E = self.grades.get("Eksāmens", 0)
-        L = self.grades.get("Laboratorijas darbi", 0)
-        K = self.grades.get("Kontroldarbs", 0)
+        print(self.grades)
+        L = [self.grades.get("1. Laboratorijas darbs", 0),
+             self.grades.get("2. Laboratorijas darbs", 0),
+             self.grades.get("3. Laboratorijas darbs", 0),
+             self.grades.get("4. Laboratorijas darbs", 0),
+             self.grades.get("5. Laboratorijas darbs", 0),
+             self.grades.get("6. Laboratorijas darbs", 0),
+             self.grades.get("7. Laboratorijas darbs", 0),
+             ]
+        
+        print(L)
+        
+        K = [self.grades.get("1. kontroldarbs 17.03.2025. Seanss 8.20", 0),
+             self.grades.get("2. kontroldarbs 19.05.2025. Seanss 8.20", 0)] # PROBLĒMA AR SEANSU !!!
+        
+        print(K)
+        #KO DARĪT AR MĀJASDARBIEM !!!!!!!!!!!!
+
+        M = [self.grades.get("Mājas darbs 1", 0),
+             self.grades.get("Mājas darbs 2", 0),
+             self.grades.get("Mājas darbs 4", 0),
+             self.grades.get("Mājas darbs 7", 0)]
+
         B = 1
-        # ja kāds mājasdarbs zem 4, tad bonuss = 0
-        for k,v in self.grades.items():
-            if "Mājasdarbs" in k and v < 4:
-                B = 0
-                break
+
+        print(B)
+        E = self.grades.get("Eksāmens", 0)
+
+        print(E)
+
+
+        L=statistics.fmean
+        K=statistics.fmean
+        B=statistics.fmean
+        E=statistics.fmean
+
+        groups = [L, K, M, B, E]
+        weights = [0.4, 0.25, 1]
+        
+        final = statistics.fmean(groups, weights)
+        return final
+
+         # BONUS PAREIZI TIEK IEVIETOTS ????
+        #ja kāds mājasdarbs zem 4, tad bonuss = 0
+        if min(M) >= 4:
+            M=B
+        else:
+            M!=B
         self.final_grade = round(0.3 * E + 0.4 * L + 0.25 * K + B, 2)
+
+      
 
 class Vides_un_klimata_celvedis(Course):
     def calculate_final(self):
-        T = self.grades.get("Testi", 0)
-        LS = self.grades.get("Lomu spēle", 0)
+        print(self.grades)
+
+        T = [self.grades.get("Uzdevums \"Elektronikas aprite\"_2", 0),
+             self.grades.get("Tests 2 - Indikatori_2", 0),
+             self.grades.get("Atjaunojamie energoresursi un viedās energosistēmas_praktiskie darbi_2", 0),
+             self.grades.get("Uzdevums \"Elektronikas aprite\"_2", 0),
+             self.grades.get("Tests - aprites cikla analīze un ekodizains_0", 0),
+             self.grades.get("Tests - aprites cikla analīze un ekodizains_1", 0),
+             self.grades.get("Tests - aprites cikla analīze un ekodizains_2", 0),
+             self.grades.get("Tests - aprites cikla analīze un ekodizains_3", 0),
+             self.grades.get("Tests 1_Aprites ekonomikas pamatprincipi _2", 0),
+             self.grades.get("Tests2_Biotehnoloģijas priekšrocības un trūkumi_2", 0),
+             self.grades.get("Ilgtspējīga mobilitāte_2", 0),
+             self.grades.get("Uzdevums_pozitīvās enerģijas pilsēta_2", 0),
+             self.grades.get("Tests1_Biotehnoloģijas_2", 0),
+             self.grades.get("Noslēguma eksāmens_2", 0),
+             ]
+        
+        print(T)
+
+        LS = [self.grades.get("Tests_Lomu spēle_2", 0)]
+
+        print(LS)
+        
         E = self.grades.get("Eksāmens", 0)
+
+        print(E)
+
+        T=statistics.fmean
+        LS=statistics.fmean
+
+        groups = [T, LS, E]
+        weights = [0.45, 0.25, 0.3]
+        final = statistics.fmean(groups, weights)
+        return final
+
         self.final_grade = round(0.45 * T + 0.25 * LS + 0.3 * E, 2)
 
 
@@ -198,12 +364,12 @@ def save_course_table(courses, filename="final_grades.txt"):
         #f.write(f"Kopējā vidējā gala atzīme: {avg_final}\n")
 
 courses = [
-    DatuStrukturas(680502, "cat_579793")
-    #DiskretaMatematika(695121, "cat_636085"),
-    #Fizika(680310, "cat_579789"),
-    #Matematika(680369, "cat_579791"),
-    #OOP(680366, "cat_579790"),
-    #Vides_un_klimata_celvedis(680306, "cat_579788"),
+    DatuStrukturas(680502, "cat_579793"),
+    DiskretaMatematika(695121, "cat_636085"),
+    Fizika(680310, "cat_579789"),
+    Matematika(680369, "cat_579791"),
+    OOP(680366, "cat_579790"),
+    Vides_un_klimata_celvedis(680306, "cat_579788"),
 ]
        
 save_course_table(courses)
